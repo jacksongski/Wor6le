10c0514c14cacd5d087f63b2a4c43f10c5acac0c
tyler7kehoe <kehoe.tyler@att.net>
2022-02-07 18:38:28 -0600

    removed enchant

--- Game.py
+++ Game.py
@@ -1,9 +1,4 @@
-import enchant
 import pandas as pd
-
-def is_english_word(word):
-    d = enchant.Dict("en_US")
-    return d.check(word)
 
 csvfile = pd.read_csv('WordList.csv', skiprows=1)
 csvWord = csvfile.sample(ignore_index=True)
